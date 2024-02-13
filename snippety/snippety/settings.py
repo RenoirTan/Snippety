8061c24bf99815f71850cb3f0374eed3e5b6a66c
RenoirTan <renoirtan2005@gmail.com>
2024-02-13 21:53:00 +0800

    main: update settings.py to use drf simple jwt

--- snippety/snippety/settings.py
+++ snippety/snippety/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
     ]
 }
