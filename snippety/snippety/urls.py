96594e098a142e3f3b055055bb19d44568771672
RenoirTan <renoirtan2005@gmail.com>
2024-02-13 22:21:10 +0800

    main: add some views for models

--- snippety/snippety/urls.py
+++ snippety/snippety/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
+
+from snippety_api import views
+
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,3 +35,5 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
+
+urlpatterns += router.urls
